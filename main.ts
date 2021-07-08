0750597e69786fb3fd06113b4eb372521a7916f9
Brian <brianhaddad@gmail.com>
2021-07-07 18:54:33 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,13 @@ namespace SpriteKind {
     export const NPC = SpriteKind.create()
 }
 namespace Sprites {
+    /**
+     * Creates an NPC sprite
+     */
+    //% blockId=spriteCreateNPC block="Create sprite with image: $image=variables_get at x: $X y: $Y"
+    //% group="Create"
+    //% weight=10
+    //% blockGap=8
     export function CreateNPC (myImage: Image, X: number, Y: number) {
         let sprite = sprites.create(myImage, SpriteKind.NPC)
         sprite.z = -1
