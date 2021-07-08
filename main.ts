138fe00e466e8d344baf734b2996d73bd671f696
Brian <brianhaddad@gmail.com>
2021-07-07 18:56:55 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -9,10 +9,9 @@ namespace Sprites {
     //% group="Create"
     //% weight=10
     //% blockGap=8
-    export function CreateNPC (myImage: Image, X: number, Y: number) {
+    export function createNPC (myImage: Image, X: number, Y: number) {
         let sprite = sprites.create(myImage, SpriteKind.NPC)
         sprite.z = -1
         tiles.placeOnTile(sprite, tiles.getTileLocation(X, Y))
-        return sprite
     }
 }
